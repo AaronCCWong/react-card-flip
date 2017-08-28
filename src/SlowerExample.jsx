917b8505944cab8c2c15283b7cb0650f9d997947
AaronCCWong <aarn.wong@gmail.com>
2017-08-28 17:10:53 -0400

    Add option to make card flip in one direction.

--- src/SlowerExample.jsx
+++ src/SlowerExample.jsx
@@ -37,7 +37,7 @@ class SlowerExample extends Component  {
         <div key="back" style={this.props.styles.card}>
           <img
             style={this.props.styles.image}
-            src="//tu9srvbirvvtmtykznrobwiudhfulmnvbq00.g00.thespruce.com/g00/2_d3d3LnRoZXNwcnVjZS5jb20%3D_/TU9SRVBIRVVTMTYkaHR0cHM6Ly9mdGhtYi50cW4uY29tL3ZacFFVMTFyS3NEUkZjLUR6TkMtbHdNTUpGaz0vOTYweDAvZmlsdGVyczpub191cHNjYWxlKCkvYWJvdXQvQ2F0LWVhdGluZy1ncmFzcy1HZXR0eUltYWdlcy01MzM1MDczODEtNThhYzYwYWUzZGY3OGMzNDViNDEzOTUzLmpwZz9pMTBjLm1hcmsuaW1hZ2UudHlwZQ%3D%3D_$/$/$/$/$/$"
+            src="//www.planwallpaper.com/static/images/Beagle-Wallpaper-dogs-7013951-1024-768.jpg"
           />
 
           <button onClick={this.handleClick}>
