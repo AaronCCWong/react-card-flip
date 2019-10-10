f78f0abdb3d6a82d56ae250879c27c69ae70554a
blackfenix2 <devin_98@msn.com>
2019-10-10 00:11:51 -0400

    added wrapper.update() method to catch prop updates on function component

--- test/ReactCardFlip.test.js
+++ test/ReactCardFlip.test.js
@@ -4,137 +4,138 @@ import { mount } from 'enzyme';
 import ReactCardFlip from '../src/ReactCardFlip.jsx';
 
 describe('Flipping', () => {
-	it('flips from front to back when props change', () => {
-		const wrapper = mount(
-			<ReactCardFlip>
-				<div key="front">
-					<p id="front_text">Front</p>
-				</div>
-				<div key="back">
-					<p id="back_text">Back</p>
-				</div>
-			</ReactCardFlip>
-		);
-		expect(wrapper.find('.react-card-front').props().style.transform).toBe(
-			'rotateY(0deg)'
-		);
-		expect(wrapper.find('.react-card-back').props().style.transform).toBe(
-			'rotateY(-180deg)'
-		);
+  it('flips from front to back when props change', () => {
+    const wrapper = mount(
+      <ReactCardFlip>
+        <div key="front">
+          <p id="front_text">Front</p>
+        </div>
+        <div key="back">
+          <p id="back_text">Back</p>
+        </div>
+      </ReactCardFlip>
+    );
+    expect(wrapper.find('.react-card-front').props().style.transform).toBe(
+      'rotateY(0deg)'
+    );
+    expect(wrapper.find('.react-card-back').props().style.transform).toBe(
+      'rotateY(-180deg)'
+    );
 
-		wrapper.setProps({ isFlipped: true });
+    wrapper.setProps({ isFlipped: true });
+    wrapper.update();
 
-		expect(wrapper.find('.react-card-front').props().style.transform).toBe(
-			'rotateY(180deg)'
-		);
-		expect(wrapper.find('.react-card-back').props().style.transform).toBe(
-			'rotateY(0deg)'
-		);
-	});
+    expect(wrapper.find('.react-card-front').props().style.transform).toBe(
+      'rotateY(180deg)'
+    );
+    expect(wrapper.find('.react-card-back').props().style.transform).toBe(
+      'rotateY(0deg)'
+    );
+  });
 
-	it('flips in the opposite direction on both sides when infinite prop is provided', () => {
-		const wrapper = mount(
-			<ReactCardFlip infinite>
-				<div key="front">
-					<p id="front_text">Front</p>
-				</div>
-				<div key="back">
-					<p id="back_text">Back</p>
-				</div>
-			</ReactCardFlip>
-		);
+  it('flips in the opposite direction on both sides when infinite prop is provided', () => {
+    const wrapper = mount(
+      <ReactCardFlip infinite>
+        <div key="front">
+          <p id="front_text">Front</p>
+        </div>
+        <div key="back">
+          <p id="back_text">Back</p>
+        </div>
+      </ReactCardFlip>
+    );
 
-		expect(wrapper.find('.react-card-front').props().style.transform).toBe(
-			'rotateY(0deg)'
-		);
-		expect(wrapper.find('.react-card-back').props().style.transform).toBe(
-			'rotateY(180deg)'
-		);
+    expect(wrapper.find('.react-card-front').props().style.transform).toBe(
+      'rotateY(0deg)'
+    );
+    expect(wrapper.find('.react-card-back').props().style.transform).toBe(
+      'rotateY(180deg)'
+    );
 
-		let isFlipped = true;
-		
-		for(let i = 1; i <= 5; i++) {
-			wrapper.setProps({ isFlipped });
-			
-			expect(wrapper.find('.react-card-front').props().style.transform).toBe(
-				`rotateY(${0 + (i * 180)}deg)`
-			);
-			expect(wrapper.find('.react-card-back').props().style.transform).toBe(
-				`rotateY(${180 + (i * 180)}deg)`
-			);
+    let isFlipped = true;
 
-			isFlipped = !isFlipped;
-		}
-	});
+    for (let i = 1; i <= 5; i++) {
+      wrapper.setProps({ isFlipped });
+      wrapper.update();
+      expect(wrapper.find('.react-card-front').props().style.transform).toBe(
+        `rotateY(${0 + i * 180}deg)`
+      );
+      expect(wrapper.find('.react-card-back').props().style.transform).toBe(
+        `rotateY(${180 + i * 180}deg)`
+      );
 
-	it('does nothing when rerendering with no isFlipped prop change', () => {
-		const wrapper = mount(
-			<ReactCardFlip isFlipped>
-				<div key="front">
-					<p id="front_text">Front</p>
-				</div>
-				<div key="back">
-					<p id="back_text">Back</p>
-				</div>
-			</ReactCardFlip>
-		);
+      isFlipped = !isFlipped;
+    }
+  });
 
-		expect(wrapper.find('.react-card-front').props().style.transform).toBe(
-			'rotateY(180deg)'
-		);
-		expect(wrapper.find('.react-card-back').props().style.transform).toBe(
-			'rotateY(0deg)'
-		);
+  it('does nothing when rerendering with no isFlipped prop change', () => {
+    const wrapper = mount(
+      <ReactCardFlip isFlipped>
+        <div key="front">
+          <p id="front_text">Front</p>
+        </div>
+        <div key="back">
+          <p id="back_text">Back</p>
+        </div>
+      </ReactCardFlip>
+    );
 
-		wrapper.setProps({ isFlipped: true });
+    expect(wrapper.find('.react-card-front').props().style.transform).toBe(
+      'rotateY(180deg)'
+    );
+    expect(wrapper.find('.react-card-back').props().style.transform).toBe(
+      'rotateY(0deg)'
+    );
 
-		expect(wrapper.find('.react-card-front').props().style.transform).toBe(
-			'rotateY(180deg)'
-		);
-		expect(wrapper.find('.react-card-back').props().style.transform).toBe(
-			'rotateY(0deg)'
-		);
-	});
+    wrapper.setProps({ isFlipped: true });
+
+    expect(wrapper.find('.react-card-front').props().style.transform).toBe(
+      'rotateY(180deg)'
+    );
+    expect(wrapper.find('.react-card-back').props().style.transform).toBe(
+      'rotateY(0deg)'
+    );
+  });
 });
 
 describe('Rendering', () => {
-	it('fails if not given two children', () => {
-		expect(() => {
-			mount(
-				<ReactCardFlip>
-					<div key="front">
-						<p id="front_text">Front</p>
-					</div>
-				</ReactCardFlip>
-			);
-		}).toThrow();
-	});
+  it('fails if not given two children', () => {
+    expect(() => {
+      mount(
+        <ReactCardFlip>
+          <div key="front">
+            <p id="front_text">Front</p>
+          </div>
+        </ReactCardFlip>
+      );
+    }).toThrow();
+  });
 
-	it('allows overriding the card container styles', () => {
-		const styles = {
-			front: {
-				position: 'absolute'
-			},
-			back: {
-				position: 'fixed'
-			}
-		};
-		const wrapper = mount(
-			<ReactCardFlip isFlipped cardStyles={styles}>
-				<div key="front">
-					<p id="front_text">Front</p>
-				</div>
-				<div key="back">
-					<p id="back_text">Back</p>
-				</div>
-			</ReactCardFlip>
-		);
+  it('allows overriding the card container styles', () => {
+    const styles = {
+      front: {
+        position: 'absolute'
+      },
+      back: {
+        position: 'fixed'
+      }
+    };
+    const wrapper = mount(
+      <ReactCardFlip isFlipped cardStyles={styles}>
+        <div key="front">
+          <p id="front_text">Front</p>
+        </div>
+        <div key="back">
+          <p id="back_text">Back</p>
+        </div>
+      </ReactCardFlip>
+    );
 
-		expect(wrapper.find('.react-card-front').props().style.position).toBe(
-			'absolute'
-		);
-		expect(wrapper.find('.react-card-back').props().style.position).toBe(
-			'fixed'
-		);
-	});
+    expect(wrapper.find('.react-card-front').props().style.position).toBe(
+      'absolute'
+    );
+    expect(wrapper.find('.react-card-back').props().style.position).toBe(
+      'fixed'
+    );
+  });
 });
